06bcc75687652ddd6d0ec30ccb507bcb0368c5cc
cpherson322 <220389203+cpherson322@users.noreply.github.com>
2025-07-29 17:25:35 +0000

    Add Mongoose database

--- Server/routes.js
+++ Server/routes.js
@@ -2,6 +2,19 @@
 
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+try {
+  mongoose.connect('mongodb://127.0.0.1:27017/test')
+} catch (e) {
+  console.error("MongoDB failed to connect", e)
+  process.exit(1)
+}
+
+let ToDoListSchema = new mongoose.Schema([{checked: Boolean, text: String}])
+let UserSchema = new mongoose.Schema({username: String, lists: {any: {type: mongoose.Schema.Types.ObjectId, ref: "ToDoList"}}})
+let UserModel = new mongoose.model('User', UserSchema)
+let ToDoListModel = new mongoose.model('ToDoList', ToDoListSchema)
 
 router.get('/', (req, res) => {
   res.status(200).send('')
@@ -17,7 +30,7 @@ router.post('/save-list', (req, res) => {
 })
 
 router.get('/get-list', (req, res) => {
-  const list = req.cookies.list || {};
+  const list = req.cookies.list || [];
   res.status(200).json(list)
 })
 
